1a303c3a0c1dd55023a4a094bae78245e57c5de2
Bonnie <0xicl33n@gmail.com>
2016-06-02 12:34:31 -0700

    Made dependencies smarter. Added mac changer

--- tools/mac_changer.py
+++ tools/mac_changer.py
@@ -1,1 +1,10 @@
-
+#!/usr/bin/python
+from os import system as sysc
+import restart_network
+class TOOL:
+	self.author = "0xicl33n"
+	self.description = "Change mac"
+	def __main__:
+		print "This tool will update kali-rolling"
+sysc('macchanger -R eth0')
+restart_network() # not sure about this
